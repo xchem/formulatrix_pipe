6c67a59a12d1c3613b2be0473991cf183856f3b0
Rachael Skyner <reskyner@googlemail.com>
2020-05-12 08:24:50 +0100

    Update get_barcodes.py

--- get_barcodes.py
+++ get_barcodes.py
@@ -98,52 +98,54 @@ class GetBarcodeInfo(luigi.Task):
         pass
 
     def output(self):
-        cwd = os.getcwd()
-        return luigi.LocalTarget(os.path.join(cwd, str('barcodes_' + str(self.plate_type)), str(self.barcode + '.csv')))
+        if self.barcode not in ['9557','954w', '9553']:
+            cwd = os.getcwd()
+            return luigi.LocalTarget(os.path.join(cwd, str('barcodes_' + str(self.plate_type)), str(self.barcode + '.csv')))
 
     def run(self):
-        # dictionary to hold info to be written out to csv for each barcode
-        results = {
-            'PlateID': [],
-            'BatchID': [],
-            'WellNum': [],
-            'ProfileID': [],
-            'WellRowLetter': [],
-            'WellColNum': [],
-            'DropNum': [],
-            'ImagerName': [],
-            'DateImaged': []
-        }
-        # connect to RockMaker DB
-        conn = pytds.connect(self.server, self.database, self.username, self.password)
-        c = conn.cursor()
-        # select all data needed by inner joining a million tables on ID's - blame formulatrix
-        c.execute(
-            "SELECT DISTINCT "
-            "w.PlateID, ib.ID, w.WellNumber, cpv.CaptureProfileID,  "
-            "w.RowLetter, w.ColumnNumber, wd.DropNumber, "
-            "crp.Value, it.DateImaged " \
-            "from Well w INNER JOIN Plate p ON w.PlateID = p.ID " \
-            "INNER JOIN WellDrop wd ON wd.WellID = w.id " \
-            "INNER JOIN ExperimentPlate ep ON ep.PlateID = p.ID " \
-            "INNER JOIN ImagingTask it ON it.ExperimentPlateID = ep.ID " \
-            "INNER JOIN ImageBatch ib ON ib.ImagingTaskID = it.ID " \
-            "INNER JOIN CaptureResult cr ON cr.ImageBatchID = ib.ID " \
-            "INNER JOIN CaptureProfileVersion cpv ON cpv.ID = cr.CaptureProfileVersionID " \
-            "INNER JOIN CaptureResultProperty crp on CaptureResultID = cr.ID " \
-            "WHERE p.Barcode=%s AND crp.Value like %s", (str(self.barcode), '%RI1000%')
-        )
-        # add all info to results dict
-        for row in c.fetchall():
-            results['PlateID'].append(str(row[0]))
-            results['BatchID'].append(str(row[1]))
-            results['WellNum'].append(str(row[2]))
-            results['ProfileID'].append(str(row[3]))
-            results['WellRowLetter'].append(str(row[4]))
-            results['WellColNum'].append(str(row[5]))
-            results['DropNum'].append(str(row[6]))
-            results['ImagerName'].append(str(row[7]))
-            results['DateImaged'].append(str(row[8].date()))
-        # write dict to output csv for image transfers
-        df = pandas.DataFrame.from_dict(results)
-        df.to_csv(self.output().path, index=False)
+        if self.barcode not in ['9557','954w', '9553']:
+            # dictionary to hold info to be written out to csv for each barcode
+            results = {
+                'PlateID': [],
+                'BatchID': [],
+                'WellNum': [],
+                'ProfileID': [],
+                'WellRowLetter': [],
+                'WellColNum': [],
+                'DropNum': [],
+                'ImagerName': [],
+                'DateImaged': []
+            }
+            # connect to RockMaker DB
+            conn = pytds.connect(self.server, self.database, self.username, self.password)
+            c = conn.cursor()
+            # select all data needed by inner joining a million tables on ID's - blame formulatrix
+            c.execute(
+                "SELECT DISTINCT "
+                "w.PlateID, ib.ID, w.WellNumber, cpv.CaptureProfileID,  "
+                "w.RowLetter, w.ColumnNumber, wd.DropNumber, "
+                "crp.Value, it.DateImaged " \
+                "from Well w INNER JOIN Plate p ON w.PlateID = p.ID " \
+                "INNER JOIN WellDrop wd ON wd.WellID = w.id " \
+                "INNER JOIN ExperimentPlate ep ON ep.PlateID = p.ID " \
+                "INNER JOIN ImagingTask it ON it.ExperimentPlateID = ep.ID " \
+                "INNER JOIN ImageBatch ib ON ib.ImagingTaskID = it.ID " \
+                "INNER JOIN CaptureResult cr ON cr.ImageBatchID = ib.ID " \
+                "INNER JOIN CaptureProfileVersion cpv ON cpv.ID = cr.CaptureProfileVersionID " \
+                "INNER JOIN CaptureResultProperty crp on CaptureResultID = cr.ID " \
+                "WHERE p.Barcode=%s AND crp.Value like %s", (str(self.barcode), '%RI1000%')
+            )
+            # add all info to results dict
+            for row in c.fetchall():
+                results['PlateID'].append(str(row[0]))
+                results['BatchID'].append(str(row[1]))
+                results['WellNum'].append(str(row[2]))
+                results['ProfileID'].append(str(row[3]))
+                results['WellRowLetter'].append(str(row[4]))
+                results['WellColNum'].append(str(row[5]))
+                results['DropNum'].append(str(row[6]))
+                results['ImagerName'].append(str(row[7]))
+                results['DateImaged'].append(str(row[8].date()))
+            # write dict to output csv for image transfers
+            df = pandas.DataFrame.from_dict(results)
+            df.to_csv(self.output().path, index=False)
