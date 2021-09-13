15d8bdaa7867d3c60d6a76a497e3c0b8ee3b38c1
Rachael Skyner <reskyner@googlemail.com>
2021-09-13 15:08:29 +0100

    use getpass to get the current user for job submission

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -1,6 +1,7 @@
 import luigi
 import os
 import subprocess
+import getpass
 
 
 def run_luigi_worker(task):
@@ -18,8 +19,10 @@ def kill_job(directory, id_file):
     with open(os.path.join(directory, id_file), 'r') as f:
         job_id = int(f.readline())
 
+    cuser = getpass.getuser()
+
     # cancel cluster job so that test of sh can be run locally
-    process = subprocess.Popen(str('ssh -t uzw12877@nx.diamond.ac.uk '
+    process = subprocess.Popen(str('ssh -t ' + cuser + '@nx.diamond.ac.uk '
                                    '"module load global/cluster >>/dev/null 2>&1; qdel ' + str(job_id) + '"'),
                                stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
 
