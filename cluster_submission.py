b7d50e6541a5dd90e75e6a6f43144e32f8a8fbad
Rachael Skyner <reskyner@googlemail.com>
2021-09-16 17:35:53 +0100

    Revert "run black on everything"
    This reverts commit 4ea9eb9699c0990322ef58046af0fac5f2133567.

--- cluster_submission.py
+++ cluster_submission.py
@@ -2,48 +2,38 @@ import subprocess
 import os
 
 
-def submit_job(job_directory, job_script, qsub_command="qsub"):
+def submit_job(job_directory, job_script, qsub_command='qsub'):
     current = os.getcwd()
     os.chdir(job_directory)
-    submission_string = f"module load global/cluster; {qsub_command} -q medium.q {job_directory}/{job_script}"
+    submission_string = f'module load global/cluster; {qsub_command} -q medium.q {job_directory}/{job_script}'
 
     print(submission_string)
-    proc = subprocess.run(
-        submission_string,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-        shell=True,
-        executable="/bin/bash",
-    )
+    proc = subprocess.run(submission_string, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, executable='/bin/bash')
     out = proc.stdout
     err = proc.stderr
 
-    out = out.decode("ascii")
-    print("\n")
+    out = out.decode('ascii')
+    print('\n')
     print(out)
-    print("\n")
+    print('\n')
     if err:
-        err = err.decode("ascii")
-        print("\n")
+        err = err.decode('ascii')
+        print('\n')
         print(err)
-        print("\n")
+        print('\n')
     os.chdir(current)
-
 
 def write_job(execute_directory, job_directory, job_filename, job_name, job_command):
     directory = os.getcwd()
     os.chdir(job_directory)
-    job_script = """#!/bin/bash
+    job_script = '''#!/bin/bash
 cd %s
 export MCR_CACHE_ROOT=$( mktemp -d )
 %s
-    """ % (
-        execute_directory,
-        job_command,
-    )
+    ''' % (execute_directory, job_command)
 
     output = os.path.join(job_directory, job_filename)
 
-    f = open(output, "w")
+    f = open(output, 'w')
     f.write(job_script)
     os.chdir(directory)
